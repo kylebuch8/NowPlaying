1802f639c4fb3ee268f43a711768646491b93001
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-10-12 21:25:47 -0400

    save files

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -1,13 +1,27 @@
 'use strict';
 
+/*
+ * iOS 7 status bar fix
+ */
+function onDeviceReady() {
+    if (parseFloat(window.device.version) === 7.0) {
+        document.body.style.marginTop = '20px';
+    }
+}
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+/*
+ * kick everything off
+ */
 angular.module('NowPlayingApp', [])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
